6c68aa958df9081ecb3ef4906b87b6cacb991c70
Anush Maharjan <anushmrzan777@gmail.com>
2024-12-13 14:05:57 +0545

    Yokoso Watashi No soul Society

--- Methods.java
+++ Methods.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+
 public class Methods {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int ans = sum(a,b);
-        System.out.println(ans);
+        System.out.println("Enter a number: ");
+        int value = takeInput();
+        printValue(value);
     }
 
     public static int sum(int fnum, int snum) { //int fnum = a & int snum = b
         int sum = fnum + snum;
         return sum;
+    }
+    public static int takeInput() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+        return a;
+    }
+    public static void printValue(int value) {
+        System.out.println("The number is " + value);
     }
 }
