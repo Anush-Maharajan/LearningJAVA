3590fb3cb649a931a14a65df4136ce988812ebfc
Anush Maharjan <anushmrzan777@gmail.com>
2025-01-19 13:52:11 +0545

    Encpasulation

---
+++ LearnClass/MyClass.java
@@ -0,0 +1,21 @@
+package LearnClass;
+
+class Encapsulatuion {
+    private int password;
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+}
+public class MyClass extends oops {
+    public static void main(String[] args) {
+        Encapsulatuion enc = new Encapsulatuion(); 
+        enc.setPassword(1234);
+        int value = enc.getPassword();
+        System.out.println(value);
+    }
+}
---
+++ LearnClass/oops.java
@@ -0,0 +1,11 @@
+package LearnClass;
+
+public class oops {
+    //static variable
+    static int counter = 1;
+
+    public void print() {
+        System.out.println(counter);
+        counter++;
+    }
+}
